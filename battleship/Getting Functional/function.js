223ec86d088cb715a81ab179b5fafcb498d78187
JayRyanG <jamesryangolocino@gmail.com>
2025-03-20 15:47:09 +0800

    new files

---
+++ battleship/Getting Functional/function.js
@@ -0,0 +1,44 @@
+function bark(name, weight) { 
+    if (weight > 20){
+        console.log(name + " says WOOF WOOF" );
+    } else {
+        console.log(name + " says woof woof" );
+    }
+}
+
+bark ("Ayam", 23);
+bark ("Antoni", 26);
+bark ("Raul", 18);
+bark ("Bogart", 18)
+
+/*var dogName = "Ayam";
+var dogWeight = 23;
+if (dogWeight > 20) {
+    console.log(dogName + "says WOOF WOOF");
+} else {
+    console.log(dogName + "says woof woof");
+}
+
+var dogName = "Bogart";
+var dogWeight = 18;
+if (dogWeight > 20) {
+    console.log(dogName + "says WOOF WOOF");
+} else {
+    console.log(dogName + "says woof woof");
+}
+
+var dogName = "Antoni";
+var dogWeight = 26;
+if (dogWeight > 20) {
+    console.log(dogName + "says WOOF WOOF");
+} else {
+    console.log(dogName + "says woof woof");
+}
+
+var dogName = "Raul";
+var dogWeight = 18;
+if (dogWeight > 20) {
+    console.log(dogName + "says WOOF WOOF");
+} else {
+    console.log(dogName + "says woof woof");
+}*/
